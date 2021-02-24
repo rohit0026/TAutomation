15f78703eefb9e45a170a60f18253817427e4eb5
rohit0026 <rupadhyay73@gmail.com>
2021-02-24 12:09:10 +0530

    commit from cloned in native appium

--- src/test/java/IMDB/Movies/AppiumTest.java
+++ src/test/java/IMDB/Movies/AppiumTest.java
@@ -13,7 +13,7 @@ public class AppiumTest {
 	@Test(groups = "Regression")
 	public void NativeApp()
 	{
-		System.out.println("Native app from cloned changes Appium tests Part of Regression");
+		System.out.println("Native1 app from cloned changes Appium tests Part of Regression");
 		//if(5>4)
 		
 	}
