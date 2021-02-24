02aa431f3046517be11f8acdbf80cceb654ba008
rohit0026 <rupadhyay73@gmail.com>
2021-02-24 11:38:52 +0530

    changes in ative-Appium test

--- src/test/java/IMDB/Movies/AppiumTest.java
+++ src/test/java/IMDB/Movies/AppiumTest.java
@@ -13,7 +13,7 @@ public class AppiumTest {
 	@Test(groups = "Regression")
 	public void NativeApp()
 	{
-		System.out.println("Native app from Appium tests Part of Regression");
+		System.out.println("Native app from cloned changes Appium tests Part of Regression");
 		//if(5>4)
 		
 	}
