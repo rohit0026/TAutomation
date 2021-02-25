7fbb5ff52357e1cbda8b8805b3bde37e3c9b0fa7
rohit0026 <rupadhyay73@gmail.com>
2021-02-25 10:25:55 +0530

    Changes from Ind team

--- src/test/java/IMDB/Movies/AppiumTest.java
+++ src/test/java/IMDB/Movies/AppiumTest.java
@@ -14,6 +14,7 @@ public class AppiumTest {
 	public void NativeApp()
 	{
 		System.out.println("Native2 from American guy app from cloned changes Appium tests Part of Regression");
+		System.out.println("Indian team commit");
 		//if(5>4)
 		
 	}
