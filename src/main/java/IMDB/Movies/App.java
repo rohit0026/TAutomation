f418e237542be758c9e633f576c2c6c4a56bb8c3
rohit0026 <rupadhyay73@gmail.com>
2021-12-04 16:19:04 +0530

    Skip to the good part

--- src/main/java/IMDB/Movies/App.java
+++ src/main/java/IMDB/Movies/App.java
@@ -1,5 +1,7 @@
 package IMDB.Movies;
 
+import org.apache.logging.log4j.core.pattern.RegexReplacement;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
+    	String s = "i";
+    	String name = "Rohit";
+    	s = s.replaceAll("i", "iiiiiiiiiii");
+    	s = s.replaceAll("i", name+ "\n");
+    	System.out.println(s);
+    	
+    	
         System.out.println( "Hello World!" );
     }
 }
